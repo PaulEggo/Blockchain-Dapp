9521af3a6bc3d477aca8333df4be77329b76bc30
PaulEggo <PaulEggo1999@gmail.com>
2021-05-31 19:23:09 +0300

    Updates

---
+++ precache-manifest.82bd3aca6530b92a79b43bd5c0392997.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b202b0bdb1b600ca3521cc6d3d4c6a7",
+    "url": "/Blockchain-Dapp/index.html"
+  },
+  {
+    "revision": "6993fa0cb1e4d856d9ce",
+    "url": "/Blockchain-Dapp/static/css/main.e9cdd8ad.chunk.css"
+  },
+  {
+    "revision": "b997388980c297f1693b",
+    "url": "/Blockchain-Dapp/static/js/2.58a75185.chunk.js"
+  },
+  {
+    "revision": "6993fa0cb1e4d856d9ce",
+    "url": "/Blockchain-Dapp/static/js/main.fa32f27d.chunk.js"
+  },
+  {
+    "revision": "72b65f20ec7e935fac88",
+    "url": "/Blockchain-Dapp/static/js/runtime-main.f092f17e.js"
+  }
+]);
